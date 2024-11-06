cdb674452989818455d4c92eeb8b719a3d8964ea
248Kobe <ecjung077@gmail.com>
2024-11-06 22:32:14 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import pic from "./pic.jpeg";
+import Component1 from "./Components/Component1";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={pic} className="App-logo" alt="logo" />
+        <br />
+        <Component1 />
       </header>
     </div>
   );
---
+++ src/Components/Component1.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Component1() {
+  return (
+    <h1>
+      22100685 정은찬 <br />
+      React Project 1
+    </h1>
+  );
+}
